24973e82b7deddf61f59655356c176481341b874
Aakura Pyakura <aakura420pyakura@gmail.com>
2019-05-17 17:45:21 +0545

    Add files via upload

---
+++ Temperature and Humidity Data Logger/Documentation.md
@@ -0,0 +1,12 @@
+# Temperature and Humidity values data logger
+
+In this project the Temperature and Humidity data collected in 30 minutes time frequency is logged to the Thingspeak™.
+
+The circuitry of this project is as here:
+![Schematic image](Weather_bb.png "Schematic Image")
+
+The components used are:
++ NodeMCU V0.9 ESP12 board
++ DHT11 Sensor
++ Green LED for Network Indicator
++ 470 Ohms Resistor for LED
---
+++ Temperature and Humidity Data Logger/src/main.cpp
@@ -0,0 +1,103 @@
+//Update 26th of Feb 2019
+//DHT11 to Thingspeak Data logging
+//Node Mcu v0.9 Lolin
+
+#include <Arduino.h>  //uncomment only in vscode
+#include <DHTesp.h>
+#include <ESP8266WiFi.h>
+
+DHTesp dht;  //dhtesp object
+
+// replace with your channel’s thingspeak API key,
+String apiKey = "LKXV6WZWXKU1KJ3Q";  //API write key of databot
+
+const char* ssid = "ranjitkarna";  //enter ssid of wifi
+const char* password = "CLB1724B3E";  //enter password of wifi
+
+const char* server = "api.thingspeak.com";
+const int DHTPIN = 14; // SO CONNECT THE DHT11/22 SENSOR TO PIN D5 OF THE NODEMCU
+//DHT dht(DHTPIN, DHT11,15); //CHANGE DHT11 TO DHT22 IF YOU ARE USING DHT22
+
+//deepsleep mode
+const int sleepTimeS = 1800; //second 30min (60*30 minute) connect reset with 16 number pin for wakeup call
+
+const int NwLed = 15; //D8 PIN
+int flag = 0;
+WiFiClient client;
+
+void setup() {
+    pinMode(NwLed, OUTPUT);
+    dht.setup(DHTPIN, DHTesp::DHT11);
+    Serial.begin(115200);
+    delay(10);
+
+    WiFi.begin(ssid, password);
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        flag = 1;
+        //ESP.reset();  //if connection timeout is faced
+    }
+    delay(2000);
+    Serial.println("");
+    Serial.println("WiFi connected");
+    flag = 0;
+
+    if(flag == 0)
+      digitalWrite(NwLed, HIGH);
+
+}
+
+void loop() {
+
+    delay(dht.getMinimumSamplingPeriod());
+    float h = dht.getHumidity();
+    float t = dht.getTemperature();
+    if (isnan(h) || isnan(t)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+
+    if (client.connect(server,80)) { // "184.106.153.149" or api.thingspeak.com
+        String postStr = apiKey;
+        postStr +="&field1=";
+        postStr += String(t);
+        postStr +="&field2=";
+        postStr += String(h);
+        postStr += "\r\n\r\n";
+
+        client.print("POST /update HTTP/1.1\n");
+        client.print("Host: api.thingspeak.com\n");
+        client.print("Connection: close\n");
+        client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+        client.print("Content-Type: application/x-www-form-urlencoded\n");
+        client.print("Content-Length: ");
+        client.print(postStr.length());
+        client.print("\n\n");
+        client.print(postStr);
+
+        Serial.print("Temperature: ");
+        Serial.print(t);
+        Serial.print(" degrees Celsius Humidity: ");
+        Serial.print(h);
+        Serial.println("% send to Thingspeak");
+        
+        delay(20000);
+        Serial.println("जादै छु deepsleep मा ");
+        ESP.deepSleep(sleepTimeS * 1000000); //deepsleep definition
+    }
+
+    client.stop(); //end the client's misery
+
+    Serial.println("Client Stopped.");
+    // thingspeak needs minimum 15 sec delay between updates
+    delay(20000);
+}
